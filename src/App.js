11d3fcf47123a4a5d0436246e601be429199245b
ryan.taherian <solymantaherian@yahoo.com>
2021-07-06 11:49:09 -0700

    test amplify auto deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Kusse nanet
+          Meso is a kussi kuss dada!
         </p>
         <a
           className="App-link"
